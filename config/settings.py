f1a35c12b8eae7539d30d96254130e966b7d10fd
XusanDev07 <133357417+XusanDev07@users.noreply.github.com>
2025-07-25 22:33:13 +0500

    Minor change made.

--- config/settings.py
+++ config/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'referral_db',
-        'USER': 'referral_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
